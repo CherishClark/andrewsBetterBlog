d525758156b99c2aacfd4363cf8d1a5e3d43e38f
Cherish Clark <cherishclark@Cherishs-MacBook-Pro.local>
2017-12-03 16:50:53 -0700

    added pages via controller-generator

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def Home
+  end
+
+  def Contact
+  end
+
+  def Shop
+  end
+
+  def More
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do |blog|
+  Blog.create!{
+    title: "Blog #{blog}",
+    subtitle: "t vero eos et accusamus et iusto",
+    body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. ",
+    post_image: "http://via.placeholder.com/350x150"
+
+
+  }
