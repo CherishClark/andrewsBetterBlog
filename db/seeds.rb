79e6c4bf130d8a4d7e4880fc0e621408fcb16af1
Cherish Clark <cherishclark@Cherishs-MacBook-Pro.local>
2017-12-03 17:04:32 -0700

    generated test bogs via seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-20.times do |blog|
-  Blog.create!{
+10.times do |blog|
+ 
+  Blog.create!(
     title: "Blog #{blog}",
     subtitle: "t vero eos et accusamus et iusto",
     body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. ",
     post_image: "http://via.placeholder.com/350x150"
+    )
+   
+end 
 
 
-  }
+  
