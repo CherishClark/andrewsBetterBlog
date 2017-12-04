aa0e937ea2b3ef5d73d8fff6a7ecea4c0a8012fa
Cherish Clark <cherishclark@Cherishs-MacBook-Pro.local>
2017-12-03 17:10:58 -0700

    corrected image placeholder for test blog posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
     title: "Blog #{blog}",
     subtitle: "t vero eos et accusamus et iusto",
     body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. ",
-    post_image: "http://via.placeholder.com/350x150"
+    post_image: "http://via.placeholder.com/350x150",
     )
    
 end 
